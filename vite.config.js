e22db87e0e98b8f70ce505f1e64f1e78dde8e769
tahanialhammad <tahani_alhammad@yahoo.com>
2025-03-14 13:27:23 +0000

    first commit

---
+++ vite.config.js
@@ -0,0 +1,49 @@
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import vue from '@vitejs/plugin-vue';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: 'resources/js/app.js',
+//             refresh: true,
+//         }),
+//         vue({
+//             template: {
+//                 transformAssetUrls: {
+//                     base: null,
+//                     includeAbsolute: false,
+//                 },
+//             },
+//         }),
+//     ],
+// });
+
+
+
+
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/js/app.js'],
+            refresh: true,
+        }),
+        vue(),
+    ],
+    build: {
+        outDir: 'public/build',
+        emptyOutDir: true,
+    },
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+        }
+    },
+    define: {
+        'process.env': {},
+    },
+});
